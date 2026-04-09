925961a68bb641712ecf1a9976f130f05ed711f0
Ashiq Renju <ashiqr.dev@gmail.com>
2026-04-09 19:17:53 +0000

    wifi added

--- components/wifi_sta/include/wifi_sta.h
+++ components/wifi_sta/include/wifi_sta.h
@@ -1,1 +1,4 @@
-#include "esp_wifi.h"
+#pragma once
+#include "esp_err.h"
+
+esp_err_t wifi_init_sta(void);
--- main/main.c
+++ main/main.c
@@ -1,4 +1,4 @@
 #include "wifi_sta.h"
 #include <stdio.h>
 
-void app_main(void) { printf("Hello world!\n"); }
+void app_main(void) { wifi_init_sta(); }
