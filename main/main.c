0202587be88e29742ac2ce9fa0278590a1a09810
Ashiq Renju <ashiqr.dev@gmail.com>
2026-04-12 10:34:01 -0400

    feat(wifi_sta): add Wi-Fi functionality (#1)
    * Add structure
    
    * wifi added
    
    * separate nvs into component

--- main/main.c
+++ main/main.c
@@ -1,3 +1,10 @@
+#include "storage.h"
+#include "wifi_sta.h"
+
 #include <stdio.h>
 
-void app_main(void) { printf("Hello world!\n"); }
+void app_main(void)
+{
+    ESP_ERROR_CHECK(storage_init());
+    wifi_init_sta();
+}
