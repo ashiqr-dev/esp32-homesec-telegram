0870f45e1f51ca760144805bd46924cab6a40146
Ashiq Renju <ashiqr.dev@gmail.com>
2026-05-02 23:24:29 -0400

    feat(wifi_sta): add Wi-Fi STA logic (#1)
    * wifi added
    
    * remove stdio

--- main/main.c
+++ main/main.c
@@ -1,3 +1,8 @@
-#include <stdio.h>
+#include "esp_err.h"
+#include "wifi_sta.h"
 
-void app_main(void) { printf("Hello world!\n"); }
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_init());
+    ESP_ERROR_CHECK(wifi_init_sta());
+}
